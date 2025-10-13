bda47a623b9091059509977296ea1d45c6e7ac35
Sanket Singh <165492193+sanketsingh01@users.noreply.github.com>
2025-10-13 14:44:25 +0000

    learne about  datatypes in js

---
+++ 04Day.js
@@ -0,0 +1,26 @@
+"use strict"; // treate al code as newer version 
+
+// alert("Hello"); This is browser fnction asnd, we are suing nodejs 
+
+console.log(3 + 3); 
+
+console.log("HItesh");
+
+// The two documentations of the JavaScrip are :- tc39 and MDN Web Docs
+
+// DataTypes in js
+
+let name = "sanket";
+
+// 1. number 
+// 2. bigInt
+// 3. String => ""
+// 4. boolena => true.false
+// 5. null => standlone value
+// 6. undefined => intentionally set value to empty
+
+// Objects 
+
+console.log(typeof name);
+console.log(typeof null); // This is famous bug in JS
+console.log(typeof undefined);
